cef9c641e363d16f49810dc0cf23760aaba77d35
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-02-09 14:11:41 +0000

    FEAT: Set up bootstrap 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Button from 'react-bootstrap/Button';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <h1>Hello, world!!</h1>
+      <Button variant="outline-success">Success</Button>
     </div>
   );
 }
