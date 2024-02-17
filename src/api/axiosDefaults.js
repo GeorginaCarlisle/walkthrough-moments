70e8679fad1f5b1abc00c97db492e985a3f73995
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-02-17 13:52:29 +0000

    FEAT: Add axios interceptors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,8 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://walk-through-django-rest-a5d1529cfa3a.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+// Below intercepts the request
+export const axiosReq = axios.create();
+// Below intercepts the response
+export const axiosRes = axios.create();
--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -1,5 +1,7 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useMemo, useState } from 'react';
 import axios from "axios";
+import { axiosReq, axiosRes } from '../api/axiosDefaults';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 // Below adds the currentUserContext (currentUser passed in as part of the return statement) to the global context (which can then be accessed by all children of app.js).
 export const CurrentUserContext = createContext();
@@ -13,14 +15,13 @@ export const useSetCurrentUSer = () => useContext(SetCurrentUserContext)
 // This function is then pulled into index.js and used to wrap the App component
 export const CurrentUserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
+    const history = useHistory;
 
   // below response from get request destructured to pull data (and data only) from the reponse
   const handleMount = async () => {
     try {
-      const {data} = await axios.get('dj-rest-auth/user/');
+      const {data} = await axiosRes.get('dj-rest-auth/user/');
       setCurrentUser(data);
-      console.log("Current user is:")
-      console.log(currentUser);
     } catch(err) {
       console.log("Error in getting current user. Error details are:")
       console.log(err);
@@ -31,6 +32,49 @@ export const CurrentUserProvider = ({ children }) => {
   useEffect(() => {
     handleMount()
   }, []);
+
+  // Below contains all logic needed for the axios interceptors
+  useMemo(() => {
+    axiosReq.interceptors.request.use(
+        async (config) => {
+            try {
+                await axios.post('/dj-rest-auth/token/refresh/')
+            } catch(err){
+                setCurrentUser((prevCurrentUser) => {
+                    if (prevCurrentUser) {
+                        history.push('/signin')
+                    }
+                    return null
+                })
+                return config
+            }
+            return config
+        },
+        (err) => {
+            return Promise.reject(err);
+        }
+    );
+
+    axiosRes.interceptors.response.use(
+        (response) => response,
+            async (err) => {
+                if (err.response?.status === 401){
+                    try{
+                        await axios.post('/dj-rest-auth/token/refresh/')
+                    } catch(err){
+                        setCurrentUser(prevCurrentUser => {
+                            if (prevCurrentUser){
+                                history.push('/signin')
+                            }
+                            return null
+                        })
+                    }
+                    return axios(err.config)
+                }
+                return Promise.reject(err)
+            }
+    );
+  }, [history]);
 
   return (
     <CurrentUserContext.Provider value={currentUser}>
