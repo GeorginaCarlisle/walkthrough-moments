ea49993696f81d6a83a318f63bb8ded9fefe2fc1
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-02-20 19:31:09 +0000

    FEAT: Popular Profiles displayed differently for mobile and desktop

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -5,7 +5,7 @@ import { axiosReq } from '../../api/axiosDefaults';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import Asset from '../../components/Asset';
 
-const PopularProfiles = () => {
+const PopularProfiles = ({mobile}) => {
   const [profileData, setProfileData] = useState({
     pageProfile: { results: [] },
     popularProfiles: { results: [] },
@@ -33,13 +33,21 @@ const PopularProfiles = () => {
   }, [currentUser]);
 
   return (
-    <Container className={appStyles.Content}>
-        <p>Most followed profiles.</p>
+    <Container className={`${appStyles.Content} ${mobile && 'd-lg-none text-center mb-3'}`}>
         {hasLoaded ? (
           <>
-            {popularProfiles.results.map(profile => (
-              <p key={profile.id}>{profile.owner}</p>
-            ))}
+            <p>Most followed profiles.</p>
+            {mobile ? (
+              <div className='d-flex justify-content-around'>
+                {popularProfiles.results.slice(0,4).map((profile) => (
+                  <p key={profile.id}>{profile.owner}</p>
+                ))}
+              </div>
+            ) : (
+              popularProfiles.results.map((profile) => (
+                <p key={profile.id}>{profile.owner}</p>
+              ))
+            )}
           </>
         ) : (
           <Container>
