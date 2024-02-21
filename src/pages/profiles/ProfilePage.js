19e5a77d71022725096b5951277828809bda4b40
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-02-21 14:42:01 +0000

    FEAT: Users can follow other profiles

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -28,7 +28,7 @@ function ProfilePage() {
   );
   const currentUser = useCurrentUser();
   const {id} = useParams();
-  const setProfileData = useSetProfileData();
+  const { setProfileData, handleFollow } = useSetProfileData();
   const { pageProfile } = useProfileData();
   const [profile] = pageProfile.results;
   const is_owner = currentUser?.username === profile?.owner;
@@ -91,7 +91,7 @@ function ProfilePage() {
             ) : (
               <Button
                 className={`${btnStyles.Button} ${btnStyles.Black}`}
-                onClick={() => {}}
+                onClick={() => handleFollow(profile)}
               >
                 follow
               </Button>
